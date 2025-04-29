5968c542b77c3d7e4a093a83fe455d42be4739a7
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-04-29 18:29:57 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+// Using import.meta.url to get the current directory
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default {
-    entry: './frontend/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        sourceType: 'module', // Explicitly define sourceType as 'module'
-                    },
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './frontend/index.html',
-            filename: 'index.html',
-        }),
+  entry: './frontend/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 3000,
-        open: true,
-    },
-    devtool: 'source-map',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './frontend/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+  },
+  devtool: 'source-map',
 };
