ef4a90848a3cc1b77fa4c240811e386a58e38ee6
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-04-29 16:29:20 +0300

    a

--- frontend/utils/style-functions.js
+++ frontend/utils/style-functions.js
@@ -2,6 +2,10 @@ export const addCSS = (path) => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = './frontend/styles/' + path + '.css';
-    document.head.appendChild(link);
+
+    const existingLink = document.querySelector(`link[href="${link.href}"]`);
+    if (!existingLink) {
+        document.head.appendChild(link);
+    }
 };
 
