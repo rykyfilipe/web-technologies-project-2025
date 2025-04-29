0568a4e1620b3ad3bb4cb2b5d0c7d8aded4366af
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-04-29 19:24:34 +0300

    asas

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,27 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');  // Adăugat pluginul pentru minimizarea CSS-ului
 
 module.exports = {
-    mode: 'production',
-    entry: './frontend/index.js',
+    mode: 'production',  // Setează modul de producție
+    entry: './frontend/index.js',  // Punctul de intrare
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        filename: 'bundle.js',  // Numele fișierului de ieșire
+        path: path.resolve(__dirname, 'dist'),  // Calea către directorul dist
+        clean: true,  // Curăță fișierele din dist înainte de a adăuga altele noi
     },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/i,  // Regula pentru fișierele CSS
+                use: ['style-loader', 'css-loader'],  // Loader-e pentru CSS
             },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.js$/,  // Regula pentru fișierele JS
+                exclude: /node_modules/,  // Exclude node_modules
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'babel-loader',  // Loader pentru Babel
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ['@babel/preset-env'],  // Preset pentru transpile la JavaScript compatibil
                     },
                 },
             },
@@ -29,20 +30,15 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './frontend/index.html',
-            filename: 'index.html',
+            template: './frontend/index.html',  // Template pentru fișierul HTML
+            filename: 'index.html',  // Numele fișierului HTML de ieșire
         }),
     ],
-    devtool: 'source-map',
-};
-
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
-module.exports = {
     optimization: {
-        minimize: true,
+        minimize: true,  // Activează minimizarea fișierelor
         minimizer: [
-            new CssMinimizerPlugin(),
+            new CssMinimizerPlugin(),  // Plugin pentru minimizarea fișierelor CSS
         ],
     },
+    devtool: 'source-map',  // Ajută la debuggarea codului sursă
 };
