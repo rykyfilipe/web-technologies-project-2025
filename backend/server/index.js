bcc9fa07852f288f5ee9d14e587ae676486884d0
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-04-29 11:53:41 +0300

    create a node server

---
+++ backend/server/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.BACKEND_PORT | 3000;
+const rootDir = path.join(__dirname, '../../');
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css'
+};
+
+http.createServer((req, res) => {
+    let filePath = path.join(rootDir, req.url === '/' ? 'index.html' : req.url);
+    let ext = path.extname(filePath);
+    let contentType = mimeTypes[ext] || 'text/plain';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('404 Not Found');
+        } else {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content);
+        }
+    });
+}).listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
