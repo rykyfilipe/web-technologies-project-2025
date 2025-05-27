d41d866de3ea5040120f79e90a0c1f23c09df54e
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-05-27 13:29:29 +0300

    creatiing data endpoint

---
+++ backend/backend-server/controllers/dataController.cjs
@@ -0,0 +1,35 @@
+/** @format */
+
+const fs = require("fs").promises;
+const path = require("path");
+
+async function interpretData() {
+	const filePath = path.join("../backend-server/data", "data.csv");
+
+	try {
+		const data = await fs.readFile(filePath, "utf8");
+		const lines = data.trim().split("\n");
+		const headers = lines[0].split(",");
+
+		const results = lines.slice(1).map((line) => {
+			const values = line.split(",");
+			const obj = {};
+			headers.forEach((header, index) => {
+				obj[header.trim()] = values[index]?.trim() || null;
+			});
+			if (obj["won"] === "True") obj["won"] = true;
+			else if (obj["won"] === "False") obj["won"] = false;
+
+			return obj;
+		});
+
+		return results;
+	} catch (err) {
+		console.error("Eroare la citirea sau parsarea fișierului:", err);
+		return null;
+	}
+}
+
+module.exports = {
+	interpretData,
+};
