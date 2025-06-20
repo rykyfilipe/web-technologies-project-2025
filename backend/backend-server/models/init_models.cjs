6291b2b81438e4f08f16798abb209808bf8320e2
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-06-20 17:40:46 +0300

    aa

--- backend/backend-server/models/init_models.cjs
+++ backend/backend-server/models/init_models.cjs
@@ -1,19 +1,18 @@
 /** @format */
 
 const mysql = require("mysql2");
-DATABASE_URL =
-	'mysql://qcmr9z02abirj67d9ttz:pscale_pw_SwWwc7j9LGuEOsRNyw4YBamwI66loTBv5eJHY25NEq9@aws.connect.psdb.cloud/wt_project?ssl={"rejectUnauthorized":true}';
 
 function createConnectionToDatabase() {
-	const connection = mysql.createConnection({
-		host: process.env.HOST,
-		user: process.env.USER,
-		password: process.env.PASSWORD,
-		database: "wt_project",
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
+	// const connection = mysql.createConnection({
+	// 	host: process.env.HOST,
+	// 	user: process.env.USER,
+	// 	password: process.env.PASSWORD,
+	// 	database: "wt_project",
+	// 	ssl: {
+	// 		rejectUnauthorized: false,
+	// 	},
+	// });
+	const connection = mysql.createConnection({ uri: process.eenv.DATABASE_URL });
 	return connection;
 }
 
