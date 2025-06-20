a814eae9b80f91bf7b8059c2eaae72a0df0ecdb9
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-06-20 18:20:11 +0300

    aa

--- frontend/sections/Login.js
+++ frontend/sections/Login.js
@@ -1,86 +1,82 @@
-import Navbar from './Navbar.js';
-import Dashboard from './Dashboard.js';
-import '../styles/Login.css';
-import '../styles/index.css';
-import RegisterUser from './RegisterUser.js';
+import Navbar from "./Navbar.js";
+import Dashboard from "./Dashboard.js";
+import "../styles/Login.css";
+import "../styles/index.css";
+import RegisterUser from "./RegisterUser.js";
 
-const url_prefix = 'http://127.0.0.1:3001'
+const url_prefix = process.env.BACKEND_URL;
 
 const Login = (container) => {
-  console.log('FUNCTIA LOGIN SE APLELEAZA');
+	console.log("FUNCTIA LOGIN SE APLELEAZA");
 
-  const loginForm = document.createElement('form');
-  loginForm.classList.add('login-form');
+	const loginForm = document.createElement("form");
+	loginForm.classList.add("login-form");
 
-  const title = document.createElement('h2');
-  title.textContent = 'Login';
-  loginForm.appendChild(title);
+	const title = document.createElement("h2");
+	title.textContent = "Login";
+	loginForm.appendChild(title);
 
-  const usernameInput = document.createElement('input');
-  usernameInput.type = 'text';
-  usernameInput.placeholder = 'Username';
-  usernameInput.name = 'username';
-  usernameInput.required = true;
-  usernameInput.classList.add('form-input');
-  loginForm.appendChild(usernameInput);
+	const usernameInput = document.createElement("input");
+	usernameInput.type = "text";
+	usernameInput.placeholder = "Username";
+	usernameInput.name = "username";
+	usernameInput.required = true;
+	usernameInput.classList.add("form-input");
+	loginForm.appendChild(usernameInput);
 
-  const passwordInput = document.createElement('input');
-  passwordInput.type = 'password';
-  passwordInput.placeholder = 'Password';
-  passwordInput.name = 'password';
-  passwordInput.required = true;
-  passwordInput.classList.add('form-input');
-  loginForm.appendChild(passwordInput);
+	const passwordInput = document.createElement("input");
+	passwordInput.type = "password";
+	passwordInput.placeholder = "Password";
+	passwordInput.name = "password";
+	passwordInput.required = true;
+	passwordInput.classList.add("form-input");
+	loginForm.appendChild(passwordInput);
 
-  const submitButton = document.createElement('button');
-  submitButton.type = 'submit';
-  submitButton.textContent = 'Login';
-  submitButton.classList.add('form-button');
-  loginForm.appendChild(submitButton);
+	const submitButton = document.createElement("button");
+	submitButton.type = "submit";
+	submitButton.textContent = "Login";
+	submitButton.classList.add("form-button");
+	loginForm.appendChild(submitButton);
 
-  const registerLink = document.createElement('a');
-  registerLink.textContent = 'Already have an account?';
-  registerLink.classList.add('form-a');
-  registerLink.addEventListener('click', async function () { 
-    loginForm.remove();
-    RegisterUser(container);
-  });
+	const registerLink = document.createElement("a");
+	registerLink.textContent = "Already have an account?";
+	registerLink.classList.add("form-a");
+	registerLink.addEventListener("click", async function () {
+		loginForm.remove();
+		RegisterUser(container);
+	});
 
-  loginForm.appendChild(registerLink);
+	loginForm.appendChild(registerLink);
 
-  loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const username = usernameInput.value;
-    const password = passwordInput.value;
+	loginForm.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const username = usernameInput.value;
+		const password = passwordInput.value;
 
-      const payload = {
-          username: username,
-          password: password,
-      };
-      
-    
-      const response = await fetch(url_prefix + '/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(payload),
-      });
+		const payload = {
+			username: username,
+			password: password,
+		};
 
-      const serverData = await response.json();
+		const response = await fetch(url_prefix + "/login", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(payload),
+		});
 
-      if (response.ok) {
-          loginForm.remove();
-          
-        Navbar(container);
-        Dashboard(container); 
-      }
-      else {
-          alert('Something incorect');
-      }
+		const serverData = await response.json();
 
-    
-  });
+		if (response.ok) {
+			loginForm.remove();
 
-  container.append(loginForm);
+			Navbar(container);
+			Dashboard(container);
+		} else {
+			alert("Something incorect");
+		}
+	});
+
+	container.append(loginForm);
 };
 
 export default Login;
--- frontend/sections/RegisterUser.js
+++ frontend/sections/RegisterUser.js
@@ -1,80 +1,80 @@
-import Navbar from './Navbar.js';
-import Dashboard from './Dashboard.js';
-import '../styles/Login.css';
-import '../styles/index.css';
-import Login from './Login.js';
+import Navbar from "./Navbar.js";
+import Dashboard from "./Dashboard.js";
+import "../styles/Login.css";
+import "../styles/index.css";
+import Login from "./Login.js";
 
-const url_prefix = 'http://127.0.0.1:3001';
+const url_prefix = process.env.BACKEND_URL;
 
 const RegisterUser = (container) => {
-  console.log('FUNCTIA REGISTER USER SE APELEAZA');
+	console.log("FUNCTIA REGISTER USER SE APELEAZA");
 
-  const registerForm = document.createElement('form');
-  registerForm.classList.add('login-form');
+	const registerForm = document.createElement("form");
+	registerForm.classList.add("login-form");
 
-  const title = document.createElement('h2');
-  title.textContent = 'Create Account';
-  registerForm.appendChild(title);
+	const title = document.createElement("h2");
+	title.textContent = "Create Account";
+	registerForm.appendChild(title);
 
-  const usernameInput = document.createElement('input');
-  usernameInput.type = 'text';
-  usernameInput.placeholder = 'Username';
-  usernameInput.name = 'username';
-  usernameInput.required = true;
-  usernameInput.classList.add('form-input');
-  registerForm.appendChild(usernameInput);
+	const usernameInput = document.createElement("input");
+	usernameInput.type = "text";
+	usernameInput.placeholder = "Username";
+	usernameInput.name = "username";
+	usernameInput.required = true;
+	usernameInput.classList.add("form-input");
+	registerForm.appendChild(usernameInput);
 
-  const passwordInput = document.createElement('input');
-  passwordInput.type = 'password';
-  passwordInput.placeholder = 'Password';
-  passwordInput.name = 'password';
-  passwordInput.required = true;
-  passwordInput.classList.add('form-input');
-  registerForm.appendChild(passwordInput);
+	const passwordInput = document.createElement("input");
+	passwordInput.type = "password";
+	passwordInput.placeholder = "Password";
+	passwordInput.name = "password";
+	passwordInput.required = true;
+	passwordInput.classList.add("form-input");
+	registerForm.appendChild(passwordInput);
 
-  const submitButton = document.createElement('button');
-  submitButton.type = 'submit';
-  submitButton.textContent = 'Sign Up';
-  submitButton.classList.add('form-button');
-  registerForm.appendChild(submitButton);
+	const submitButton = document.createElement("button");
+	submitButton.type = "submit";
+	submitButton.textContent = "Sign Up";
+	submitButton.classList.add("form-button");
+	registerForm.appendChild(submitButton);
 
-  const loginLink = document.createElement('a');
-  loginLink.textContent = 'Already have an account?';
-  loginLink.classList.add('form-a');
-  loginLink.addEventListener('click', async function () {
-    registerForm.remove();
-    Login(container);
-  });
-    registerForm.appendChild(loginLink);
+	const loginLink = document.createElement("a");
+	loginLink.textContent = "Already have an account?";
+	loginLink.classList.add("form-a");
+	loginLink.addEventListener("click", async function () {
+		registerForm.remove();
+		Login(container);
+	});
+	registerForm.appendChild(loginLink);
 
-  registerForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const username = usernameInput.value;
-    const password = passwordInput.value;
+	registerForm.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const username = usernameInput.value;
+		const password = passwordInput.value;
 
-    const payload = {
-      username: username,
-      password: password,
-    };
+		const payload = {
+			username: username,
+			password: password,
+		};
 
-    const response = await fetch(url_prefix + '/register-user', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload),
-    });
+		const response = await fetch(url_prefix + "/register-user", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(payload),
+		});
 
-    const serverData = await response.json();
+		const serverData = await response.json();
 
-    if (response.ok) {
-      registerForm.remove();
+		if (response.ok) {
+			registerForm.remove();
 
-      Navbar(container);
-      Dashboard(container);
-    } else {
-      alert('Something incorect');
-    }
-  });
-  container.append(registerForm);
+			Navbar(container);
+			Dashboard(container);
+		} else {
+			alert("Something incorect");
+		}
+	});
+	container.append(registerForm);
 };
 
 export default RegisterUser;
