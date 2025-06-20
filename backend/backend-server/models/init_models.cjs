801313e46c0e5d6d9685f873235b5b4edb8c0690
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-06-20 17:49:25 +0300

    aa

--- backend/backend-server/models/init_models.cjs
+++ backend/backend-server/models/init_models.cjs
@@ -3,16 +3,15 @@
 const mysql = require("mysql2");
 
 function createConnectionToDatabase() {
-	// const connection = mysql.createConnection({
-	// 	host: process.env.HOST,
-	// 	user: process.env.USER,
-	// 	password: process.env.PASSWORD,
-	// 	database: "wt_project",
-	// 	ssl: {
-	// 		rejectUnauthorized: false,
-	// 	},
-	// });
-	const connection = mysql.createConnection({ uri: process.env.DATABASE_URL });
+	const connection = mysql.createConnection({
+		host: process.env.HOST,
+		user: process.env.USER,
+		password: process.env.PASSWORD,
+		database: "wt_project",
+		ssl: {
+			rejectUnauthorized: true,
+		},
+	});
 	return connection;
 }
 
