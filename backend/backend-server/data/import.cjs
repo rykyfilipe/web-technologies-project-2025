e7581f6003e130b55a4a111ec31b512cafd4c0a0
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-06-25 12:16:17 +0300

    creare script de populare a bazei de date cu info destre actori, filme si nominalizari din fisierul .csv

--- backend/backend-server/data/import.cjs
+++ backend/backend-server/data/import.cjs
@@ -3,13 +3,14 @@ const path = require("path");
 const mysql = require("mysql2/promise");
 
 const dbConfig = {
-	host: "aws.connect.psdb.cloud",
-	user: "0dj90vr4qatwxytjiicl",
-	password: "pscale_pw_KrBAW0Ipwl6vN6j71uWJvjvMJULiVFNCML4fnp7X0e7",
-	database: "wt_project",
-	ssl: {
-		rejectUnauthorized: false,
-	},
+	host: process.env.HOST,
+		user: process.env.USER,
+		password: process.env.PASSWORD,
+		database: "wt_project",
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 };
 
 async function getOrCreateActor(conn, name) {
